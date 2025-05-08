786c847f0f4365abab115dc30bf9d3567c5d95a3
coorghari <43966833+coorghari@users.noreply.github.com>
2025-05-08 10:27:31 +0530

    Update the console changes in the main.js file.

--- main.js
+++ main.js
@@ -281,17 +281,4 @@ function findMaxSumInArrayShort(arr) {
 
 console.log(findMaxSumInArrayShort([-1, 2, 3, -9, 11]));
 console.log("Hello");
-console.log("Hello");
-console.log("Hello");
-console.log("Hello");
-console.log("Hello");
-console.log("Hello");
-console.log("Hello");
-console.log("Hello");
-console.log("Hello");
-console.log("Hello");
-console.log("Hello");
-console.log("Hello");
-console.log("Hello");
-console.log("Hello");
 //http://127.0.0.1:5500/
