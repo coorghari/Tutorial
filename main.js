64a4e0b3a1744849feb47dd6b1e90a18246d9aca
coorghari <43966833+coorghari@users.noreply.github.com>
2025-05-08 15:25:35 +0530

    Update main.js removed console

--- main.js
+++ main.js
@@ -286,12 +286,4 @@ console.log("Hello");
 console.log("Hello");
 console.log("Hello");
 console.log("Hello");
-console.log("Hello");
-console.log("Hello");
-console.log("Hello");
-console.log("Hello");
-console.log("Hello");
-console.log("Hello");
-console.log("Hello");
-console.log("Hello");
 //http://127.0.0.1:5500/
